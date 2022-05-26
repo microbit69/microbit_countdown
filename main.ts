ad9ab282ac58f4b71c68013f5fe66c1854821fb5
microbit69 <106347913+microbit69@users.noreply.github.com>
2022-05-26 22:38:49 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
-for (let Index = 0; Index <= 2; Index++) {
-    music.playTone(262, music.beat(BeatFraction.Quarter))
-    basic.showNumber(3 - Index)
-}
-music.playTone(392, music.beat(BeatFraction.Whole))
-basic.showString("GIAN")
-music.playMelody("E G C5 A B G A F ", 120)
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+})
 basic.forever(function () {
-	
+    for (let Index = 0; Index <= 100; Index++) {
+        basic.showNumber(Index)
+    }
 })
